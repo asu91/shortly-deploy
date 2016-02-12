7af68862319891fb2047db9638a10ca974ca7d56
Hack Reactor Students <accounts@hackreactor.com>
2016-02-11 16:22:40 -0800

    implemented structure for mongodb refactor

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,35 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var mongoose = require('mongoose');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function() {
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function() {
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+
+var userSchema = mongoose.Schema({
+  username: {type: String, index:{ unique: true }},
+  password: String
 });
+
+var User = mongoose.model('User', userSchema);
+
+
+userSchema.pre('save', function(next){
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
+    });
+});
+
+User.prototype.comparePassword = function(password, callback){
+  bcrypt.compare(password, this.password, function(err, match){
+    if(err){
+      callback(err, null);
+    } else {
+      callback(null, match);
+    }
+  });
+};
+
 
 module.exports = User;
